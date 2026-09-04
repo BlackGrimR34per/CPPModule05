4110a11048b8146ca10a592063abfab7d4bcd007
Yohen Sheraun <black@Yohens-MacBook-Air.local>
2026-09-04 21:09:47 +0800

    bug: fixed string of filename being std::string to c str

--- ex03/srcs/ShrubberyCreationForm.cpp
+++ ex03/srcs/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/07 20:54:55 by yosherau          #+#    #+#             */
-/*   Updated: 2026/02/10 17:16:13 by yosherau         ###   ########.fr       */
+/*   Updated: 2026/09/04 21:05:42 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,7 @@ ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationF
 
 void	ShrubberyCreationForm::executeAction(void) const
 {
-	std::ofstream	outFile(target + "_shrubbery");
+	std::ofstream	outFile((target + "_shrubbery").c_str());
 	std::ifstream	inputFile("art.txt");
 
 	if (!outFile.is_open())
